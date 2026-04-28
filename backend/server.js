3655ad5a53d33a41481de564468cc5afb0967adf
Andre Almeida <andrealmeida@Andres-MacBook-Air.local>
2026-04-27 21:08:50 -0300

    Fix CORS for www domain

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "*",
+  origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, process.env.FRONTEND_URL.replace('https://', 'https://www.')] : "*",
   credentials: true
 }))
 app.use(express.json())
